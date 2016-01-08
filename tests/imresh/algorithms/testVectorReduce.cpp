c62a299b758995c404ed9e95373982549d352e99
mxmlnkn <mxmlnkn@github.de>
2016-01-08 22:57:23 +0100

    applied folder structure to tests folder

--- tests/imresh/algorithms/testVectorReduce.cpp
+++ tests/imresh/algorithms/testVectorReduce.cpp
@@ -36,93 +36,13 @@
 #include "algorithms/vectorReduce.hpp"
 #include "algorithms/cuda/cudaVectorReduce.hpp"
 #include "libs/cudacommon.h"
+#include "benchmarkHelper.hpp"
 
 
 namespace imresh
 {
-namespace tests
+namespace algorithms
 {
-
-
-    std::vector<int> getLogSpacedSamplingPoints
-    (
-        const unsigned & riStartPoint,
-        const unsigned & riEndPoint,
-        const unsigned & rnPoints
-    )
-    {
-        assert( riStartPoint > 0 );
-        assert( riEndPoint > riStartPoint );
-        assert( rnPoints > 0 );
-        assert( rnPoints <= riEndPoint-riStartPoint+1 );
-
-        std::vector<float> naivePoints( rnPoints );
-        std::vector<int> tmpPoints( rnPoints );
-        std::vector<int> points( rnPoints );
-
-        /* Naively create logspaced points rounding float to int */
-        const float dx = ( logf(riEndPoint) - logf(riStartPoint) ) / rnPoints;
-        const float factor = exp(dx);
-        //std::cout << "dx = " << dx << ", factor = " << factor << "\n";
-
-        naivePoints[0] = riStartPoint;
-        for ( unsigned i = 1; i < rnPoints; ++i )
-            naivePoints[i] = naivePoints[i-1]*factor;
-        /* set last point manually because of rounding errors */
-        naivePoints[ naivePoints.size()-1 ] = riEndPoint;
-
-        //std::cout << "naivePoints = ";
-        //for ( const auto & elem : naivePoints )
-        //    std::cout << elem << " ";
-        //std::cout << "\n";
-
-        /* sift out values which appear twice */
-        tmpPoints[0] = (int) naivePoints[0];
-        int iTarget = 1;
-        for ( unsigned i = 1; i < rnPoints; ++i )
-        {
-            if ( tmpPoints[ iTarget-1 ] != (int) naivePoints[i] )
-                tmpPoints[ iTarget++ ] = (int) naivePoints[i];
-        }
-
-        //std::cout << "tmpPoints = ";
-        //for ( const auto & elem : tmpPoints )
-        //    std::cout << elem << " ";
-        //std::cout << "\n";
-
-        /* if converted to int and fill in as many values as deleted */
-        int nToInsert = rnPoints - iTarget;
-        points[0] = tmpPoints[0];
-        iTarget = 1;
-        //std::cout << "nToInsert = " << nToInsert << "\n";
-        for ( unsigned iSrc = 1; iSrc < rnPoints; ++iSrc )
-        {
-            for ( ; nToInsert > 0 and points[iTarget-1] < tmpPoints[iSrc]-1;
-                  ++iTarget, --nToInsert )
-            {
-                assert( iTarget < points.size() );
-                points[iTarget] = points[iTarget-1] + 1;
-            }
-            assert( iSrc    < tmpPoints.size() );
-            //assert( iTarget < points.size() );
-            /* this condition being false should only happen very rarely, if
-             * rnPoints == riEndPoint - riStartPoint + 1 */
-            if ( iTarget < points.size() )
-                points[iTarget++] = tmpPoints[iSrc];
-        }
-
-        //std::cout << "points = ";
-        //for ( const auto & elem : points )
-        //    std::cout << elem << " ";
-        //std::cout << "\n";
-
-        for ( unsigned i = 1; i < points.size(); ++i )
-        {
-            assert( points[i-1] < points[i] );
-        }
-
-        return points;
-    }
 
 
     void testVectorReduce( void )
@@ -164,6 +84,7 @@ namespace tests
         using clock = std::chrono::high_resolution_clock;
 
         std::cout << "vector length : cudaVectorMax | vectorMax | cudaVectorMin | vectorMin\n";
+        using namespace imresh::tests;
         for ( auto nElements : getLogSpacedSamplingPoints( 2, nMaxElements, 50 ) )
         {
             std::cout << std::setw(8) << nElements << " : ";
@@ -250,11 +171,11 @@ namespace tests
     }
 
 
-} // namespace tests
+} // namespace algorithms
 } // namespace imresh
 
 
 int main( void )
 {
-    imresh::tests::testVectorReduce();
+    imresh::algorithms::testVectorReduce();
 }
