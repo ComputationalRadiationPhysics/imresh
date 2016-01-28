864763a8780455613d8e3aa1d2754a7be3eae4d6
Philipp Trommler <philipp.trommler@mailbox.tu-dresden.de>
2016-01-28 16:21:48 +0100

    Finishing touches on #27

--- src/imresh/io/readInFuncs/readInFuncs.hpp
+++ src/imresh/io/readInFuncs/readInFuncs.hpp
@@ -41,38 +41,41 @@ namespace readInFuncs
      * Simple function for reading txt files.
      *
      * They need to store their values as a 2D matrix with spaces as delimiters.
+     * _Note:_ This is currently not working!
      */
-    std::pair<float *, std::pair<unsigned int, unsigned int> >
+    std::pair<float*,std::pair<unsigned int,unsigned int>>
     readTxt
     (
-        std::string const _filename
+        std::string const& _filename
     );
 
 #   ifdef USE_PNG
         /**
-         * Reads date from PNG
+         * Reads data from PNG.
          *
          * @param[in] path to filename
          * @return pair where first contains pointer to data. NULL on error
          *         e.g. allocation problem or couldn't open file.
          *         second pair element contains 2d dimension values as pair.
          *         They are 0 on error.
-         **/
-        std::pair<float *, std::pair<unsigned int,unsigned int> >
+         */
+        std::pair<float*,std::pair<unsigned int,unsigned int>>
         readPNG
         (
-            std::string const _filename
+            std::string const& _filename
         );
 #   endif
 
 #   ifdef USE_SPLASH
         /**
          * @see readPNG
+         *
+         * _Note:_ This is currently not working!
          **/
-        std::pair<float *,std::pair<unsigned int,unsigned int> >
+        std::pair<float*,std::pair<unsigned int,unsigned int>>
         readHDF5
         (
-            std::string const _filename
+            std::string const& _filename
         );
 #   endif
 
